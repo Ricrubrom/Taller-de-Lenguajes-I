43ca600f08218fcc74dd2462f102b27643753c97
Mateo Romero <materomero04@gmail.com>
2023-03-17 17:45:10 -0300

    Ejercicios extra 4 y 5(casi terminado)

--- Practica-1/Ejercicios Extra/EjercicioE4.c
+++ Practica-1/Ejercicios Extra/EjercicioE4.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
+
+int suma(int num);
+
 int main(){
-  
+  int num;
+  do
+  {
+    printf("\nInserte un numero entero (0 Finaliza la carga): ");
+    scanf("%d", &num);
+    if (num!=0)
+      suma(num);
+  } while (num!=0);
   return 0;
+}
+
+int suma(int num){
+  int aux;
+  printf("\n%d", num);
+  while(num>=10){
+    aux=0;
+    while (num!=0)
+    {
+    aux += (num % 10);
+    num /= 10;
+    }
+  printf(" -> %d", aux);
+  num = aux;
+  }
+  return aux;
 }
---
+++ Practica-1/Ejercicios Extra/EjercicioE5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(){
+  srand(time(0));
+  int dados[2];
+  int primerDados;
+  dados[0] = rand() % 6 + 1;
+  dados[1] = rand() % 6 + 1;
+  primerDados = dados[0] + dados[1];
+  if ((primerDados==7)||(primerDados==11)){
+    printf("Felicidades, has ganado con %d", primerDados);
+  }
+  else while (1){
+    if (((dados[0]+dados[1])==2)||((dados[0]+dados[1])==12)){
+      printf("Perdiste con %d", dados[0] + dados[1]);
+    }
+  }
+  while (1) {
+    
+  }
+  return 0;
+}
